5054c8f4223802db72aa791816d6db3e834976ad
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-05-25 00:18:27 -0400

    working scoreboard, minimum project requirement reached

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,14 @@
 */
 
 //Global Variables
-var turn = 1; // Variable keep track of game phase
 var messageList = $("#messageList");
 var playerName = "";
+var playerNode = null;
 var makePlayer1 = true;
+var playerChoice;
+var opponentChoice;
+var winScore = 0;
+var loseScore = 0;
 
 //variables to determine which player you are
 var isPlayer1 = false;
@@ -34,17 +38,16 @@ database.ref('/players').on('value', function(snapshot){
     var p1 = snapshot.child('1');
     var p2 = snapshot.child('2');
     if (p1.exists() && p2.exists()){
-        if (!isPlayer1 || !isPlayer2){
-            $("#gameInfo").text("Game in Session");
+        if (!isPlayer1 && !isPlayer2){
+            $("#gameInfo").html("<p>Game in Session</P>");
         };
-        console.log("game full");
+        //console.log("game full");
         /* Run Game display both p1 and p2*/
         $("#p1Name").text(p1.val().playerName);
         $("#p1Score").text("Wins: " + p1.val().win + " Losses: " + p1.val().loss);
         $("#p2Name").text(p2.val().playerName);
         $("#p2Score").text("Wins: " + p2.val().win + " Losses: " + p2.val().loss);
         database.ref().update({phase: 1});
-        console.log(p1.val().choice == null);
         if(isPlayer1){
             playerChoice = p1.val().choice;
             opponentChoice = p2.val().choice
@@ -53,23 +56,22 @@ database.ref('/players').on('value', function(snapshot){
             playerChoice = p2.val().choice;
             opponentChoice = p1.val().choice
         }
-        rpsGame(playerChoice, opponentChoice);
+        rpsGame();
     }
     else{
         if(p1.exists()){
             /*Make Player 2 display player 1*/
             //console.log("making player 1")
             makePlayer1 = false;
-            isPlayer2 = true;
             $("#p1Name").text(p1.val().playerName);
             $("#p1Score").text("Wins: " + p1.val().win + " Losses: " + p1.val().loss);
             $("#p2Name").text("Waiting for Player 2");
             $("#p2Score").empty();
+            $("#p1Zone").empty();
         }
         else {
             /*Make Player 1*/
             makePlayer1 = true;
-            isPlayer1 = true;
             if(p2.exists()){
                 /*display player2*/
                 //console.log("displaying player 2")
@@ -77,6 +79,7 @@ database.ref('/players').on('value', function(snapshot){
                 $("#p2Score").text("Wins: " + p2.val().win + " Losses: " + p2.val().loss);
                 $("#p1Name").text("Waiting for Player 1");
                 $("#p1Score").empty();
+                $("#p2Zone").empty();
             }
             else{
                 //Clears database if no players exists
@@ -85,7 +88,7 @@ database.ref('/players').on('value', function(snapshot){
         }
     }
     //DEBUG CODE REMOVE WHEN DONE
-    console.log(snapshot.val());
+    //console.log(snapshot.val());
 });
 
 database.ref('/chat').on('child_added', function(chatsnapshot){
@@ -103,8 +106,23 @@ database.ref('/chat').on('child_added', function(chatsnapshot){
 });
 
 database.ref('/phase').on('value', function(phaseShot){
-    //Listen event for phase change
+    //Listen event for phase change, update scores
     console.log(phaseShot.val());
+    //var p1Choice = playerChoice;
+    //var p2Choice = opponentChoice;
+    if (phaseShot.val()==2){
+        var status = compareChoice();
+        database.ref('/players/'+ playerNode).update({choice : null});
+        database.ref().update({phase: 1});
+        if (status == 'win'){
+            database.ref('/players/'+ playerNode).update({win : winScore});
+            console.log("win");
+        }
+        else if (status == "lose"){
+            database.ref('/players/'+ playerNode).update({loss : loseScore});
+            console.log('lose');
+        }
+    }
 });
 
 //Functions
@@ -119,22 +137,23 @@ function sendChatMessage(message){
 
 function assignPlayer(){
     //Function assigns player as either player 1 or 2
-    var key;
     switch(makePlayer1){
         case true:
-            key = "1";
+            playerNode = "1";
+            isPlayer1 = true;
             break;
         case false:
-            key = "2";
+            playerNode = "2";
+            isPlayer2 = true;
     }
 
-    var p = $("<p>").text("Hello: " + playerName + "! you are Player" + key);
+    var p = $("<p>").text("Hello: " + playerName + "! you are Player" + playerNode);
     $("#gameInfo").append(p);
-    var playerDataRef = database.ref('/players').child(key);
+    var playerDataRef = database.ref('/players').child(playerNode);
     playerDataRef.set({
         playerName: playerName,
-        win: 0,
-        loss: 0,
+        win: winScore,
+        loss: loseScore,
         choice: null
     });
     sendChatMessage("Has joined the game.");
@@ -143,31 +162,138 @@ function assignPlayer(){
     var newPost = database.ref().child('chat').push().key;
     var updates = {};
     updates['/chat/'+newPost] = {playerName: playerName, message: "Disconnected"};
-    updates['/players/'+key] = null;
+    updates['/players/'+ playerNode] = null;
+    updates['/phase'] = null;
     database.ref().onDisconnect().update(updates);
 }
 
-function rpsGame(playerChoice, opponentChoice){
+function rpsGame(){
     //Function that holds the core game logic
     if ((playerChoice != null) && (opponentChoice != null)){
         //both choices have been made
-        database.ref().update({phase: 2});
+        displayChoice();
+        setTimeout(function(){
+            database.ref().update({phase: 2});
+        }, 3000);
+    }
+    else{      
+        if(playerChoice == null && isPlayer1){
+            buildButtons(1);
+        }
+        else if(playerChoice == null && isPlayer2){
+            buildButtons(2);
+        }
+    }
+}
+
+function buildButtons(player){
+    //Function dynamically builds the Rock Paper Scissor Choices
+    var gameList = ['Rock', 'Paper', 'Scissor'];
+    if (player == 1){
+        var playerZone = $("#p1Zone");
+    }
+    else if(player ==2){
+        var playerZone = $("#p2Zone");
     }
     else{
-        var gameList = ['Rock', 'Paper', 'Scissor'];
-        
+        console.log("ERROR not a player");
     }
+    $("#p1Zone").empty();
+    $("#p2Zone").empty();
+    for (i=0; i<gameList.length; i++){
+        var button = $("<button>");
+        button.attr({
+            class: "btn rpsButton",
+            'data-name': gameList[i],
+        });
+        button.text(gameList[i]);
+        playerZone.append(button);
+    }
+}
+
+function compareChoice(){
+    //Function compares player choices and returns 'win' 'lose' or 'draw'
+    switch(playerChoice){
+        case 'Rock':
+            switch(opponentChoice){
+                case 'Scissor':
+                    winScore++;
+                    return 'win';
+                case 'Paper':
+                    loseScore++;
+                    return 'lose';
+            }
+            break;
+        case 'Paper':
+            switch(opponentChoice){
+                case 'Rock':
+                    winScore++;
+                    return 'win';
+                case 'Scissor':
+                    loseScore++;
+                    return 'lose';
+            }
+            break;
+        case 'Scissor':
+            switch(opponentChoice){
+                case 'Paper':
+                    winScore++;
+                    return 'win';
+                case 'Rock':
+                    loseScore++;
+                    return 'lose';
+            }
+            break;
+        default:
+            return 'draw';
+    }
+}
+
+function displayChoice(){
+    if (isPlayer1){
+        var p1 = playerChoice;
+        var p2 = opponentChoice;
+    }
+    else{
+        var p2 = playerChoice;
+        var p1 = opponentChoice;
+    }
+    $("#p1Zone").empty();
+    $("#p1Zone").html("<h5>" + p1 + "</h5>");
+
+    $("#p2Zone").empty();
+    $("#p2Zone").html("<h5>" + p2 + "</h5>");
 }
 
 //Main
 //Shorthand for $(document).ready(function(){...});
 $(function(){
     //Click event for username submit
-    $("#addPlayer").on("click", function(){
+    $("#addPlayer").on("click", function(event){
         event.preventDefault();
         playerName = $("#playerName").val();
+        winScore = 0;
+        loseScore = 0;
         $("#gameInfo").empty();
         assignPlayer();
+    });
+
+    //Click event for RPS buttons and updates database of choice
+    $(".gameBox").on('click', '.rpsButton', function(){
+        var choice = $(this).attr("data-name")
+        if (isPlayer1){
+            database.ref('players/1').update({choice: choice})
+            $("#p1Zone").empty();
+            $("#p1Zone").html("<h5>" + choice + "</h5>");
+        }
+        else if (isPlayer2){
+            database.ref('players/2').update({choice: choice})
+            $("#p2Zone").empty();
+            $("#p2Zone").html("<h5>" + choice + "</h5>");
+        }
+        else{
+            console.log("ERROR not a player choice");
+        }
     });
 
     //Click event for message field submit
